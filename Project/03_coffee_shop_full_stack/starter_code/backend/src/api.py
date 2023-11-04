c86f02506ed23ea90de767b58813f59128a85904
Vuong Manh <vgtuanmanh@outlook.com>
2023-11-04 18:10:00 +0800

    Submit
    Completed all TODOs

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -1,3 +1,6 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
 import os
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
@@ -7,9 +10,22 @@ from flask_cors import CORS
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
 
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
+
+# Set up CORS. Allow '*' for origins.
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Use the after_request decorator to set Access-Control-Allow
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 '''
 @TODO uncomment the following line to initialize the datbase
@@ -17,9 +33,19 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all(app)
 
-# ROUTES
+
+#----------------------------------------------------------------------------#
+# ROUTES.
+#----------------------------------------------------------------------------#
+# index
+@app.route('/')
+def index():
+    return jsonify({
+        "success":True
+    })
+
 '''
 @TODO implement endpoint
     GET /drinks
@@ -28,6 +54,24 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+#----------------------------------------------------------------------------#
+# GET /drinks
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    try:
+        # Query from DB
+        drinks = Drink.query.all()
+        if not drinks:
+            abort(404)
+
+        # Return drinks in JSON format
+        short_drinks = [drink.short() for drink in drinks]
+        return jsonify({
+            'success': True,
+            'drinks': short_drinks
+        }), 200
+    except Exception as error:
+        abort(500)
 
 
 '''
@@ -38,6 +82,26 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+#----------------------------------------------------------------------------#
+# GET /drinks-detail
+@app.route('/drinks-detail',methods=['GET'])
+@requires_auth('get:drinks-detail')
+def drinks_long(payload):
+    try:
+        # Query from DB
+        drinks = Drink.query.all()
+        if not drinks:
+            abort(404)
+
+        # Return drinks in JSON format
+        long_drinks = [drink.long() for drink in drinks]
+        return jsonify({
+            'success': True,
+            'drinks': long_drinks
+        }), 200
+
+    except Exception as error:
+        abort(500)
 
 
 '''
@@ -49,6 +113,24 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+#----------------------------------------------------------------------------#
+# POST /drinks
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def post_drink(payload):
+    try:
+        # get data and insert db
+        form = request.get_json()
+        drink = Drink(title=form['title'],recipe=json.dumps(form['recipe']))
+        drink.insert()
+
+    except Exception as error:
+        abort(422)
+
+    return jsonify({
+        "success":True,
+        "test":drink.long()
+    }), 200
 
 
 '''
@@ -62,6 +144,33 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+#----------------------------------------------------------------------------#
+# PATCH /drinks/<id>
+@app.route("/drinks/<id>",methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drink(payload, id):
+    try:
+        # DB query
+        drink = Drink.query.get(id)
+
+        if not drink:
+            abort(404)
+
+        # processing
+        form = request.get_json()
+        if 'title' in form:
+            drink.title = form['title']
+        if 'recipe' in form:
+            drink = json.dumps(form['recipe'])
+        drink.update()
+
+    except Exception as error:
+        abort(422)
+
+    return jsonify({
+        "success": True,
+        "drinks": [drink.long()],
+    }), 200
 
 
 '''
@@ -74,14 +183,34 @@ CORS(app)
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+#----------------------------------------------------------------------------#
+@app.route("/drinks/<id>",methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    try:
+        # DB query
+        matching_drink = Drink.query.get(id)
+
+        if not matching_drink:
+            abort(404)
+
+        matching_drink.delete()
+
+    except Exception as error:
+        abort(422)
+
+    return jsonify({
+        "success": True,
+        "delete": id
+    }), 200
 
 
+#----------------------------------------------------------------------------#
 # Error Handling
 '''
 Example error handling for unprocessable entity
 '''
-
-
+# error handler for 422 (Unprocessable Entity)
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
@@ -106,9 +235,40 @@ def unprocessable(error):
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
+# error handler for 404 (Not Found)
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Resource not found'
+    }), 404
 
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+# error handler for 400 (Bad Request)
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'Bad Request'
+    }), 400
+
+# error handler for 500 (Internal Server Error)
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        'success': False,
+        'error': 500,
+        'message': 'Internal Server Error'
+    }), 500
--- Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -1,13 +1,20 @@
+import os
 import json
 from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+from dotenv import find_dotenv, dotenv_values
 
+## Loading variable
+ENV_FILE = find_dotenv(raise_error_if_not_found = True)
+if ENV_FILE:
+    env = dotenv_values(ENV_FILE)
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+ALGORITHMS = os.getenv('ALGORITHMS')
+API_AUDIENCE = os.getenv('API_AUDIENCE')
+
 
 ## AuthError Exception
 '''
@@ -31,7 +38,30 @@ class AuthError(Exception):
     return the token part of the header
 '''
 def get_token_auth_header():
-   raise Exception('Not Implemented')
+    # Check if the "Authorization" header is present in the request
+    if 'Authorization' not in request.headers:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is required.'
+        }, 401)
+
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split()
+
+    # Check if the header has two parts (Bearer and token)
+    if len(header_parts) != 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must have two parts.'
+        }, 401)
+    elif header_parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    token = header_parts[1]
+    return token
 
 '''
 @TODO implement check_permissions(permission, payload) method
@@ -45,7 +75,19 @@ def get_token_auth_header():
     return true otherwise
 '''
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permissions not included in the JWT.'
+        }, 403)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
+
+    return True
 
 '''
 @TODO implement verify_decode_jwt(token) method
@@ -61,8 +103,57 @@ def check_permissions(permission, payload):
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
 
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e'],
+            }
+
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/',
+            )
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token is expired.'
+            }, 401)
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+        'code': 'invalid_header',
+        'description': 'Unable to find the appropriate key.'
+    }, 403)
 '''
 @TODO implement @requires_auth(permission) decorator method
     @INPUTS
